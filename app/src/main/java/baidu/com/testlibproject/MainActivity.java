fce495d31e31189ce60358bd7db235654c8f6839
Wayne <baidu@WaynedeMacBook-Pro.local>
2015-11-01 15:54:53 +0800

    change : initial project.

---
+++ app/src/main/java/baidu/com/testlibproject/MainActivity.java
@@ -0,0 +1,13 @@
+package baidu.com.testlibproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
