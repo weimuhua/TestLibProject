c3604f4a799a3a908b2d92f605a2fd474eff44dd
weimuhua <weimuhua0812@126.com>
2015-11-02 19:56:43 +0800

    change : add keystore and HttpUtils test.

--- app/src/main/java/baidu/com/testlibproject/MainActivity.java
+++ app/src/main/java/baidu/com/testlibproject/MainActivity.java
@@ -2,12 +2,42 @@ package baidu.com.testlibproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import baidu.com.commontools.http.HttpUtils;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+    private static final boolean DEBUG = FeatureConfig.DEBUG;
+
+    private static final String URL = "http://www.baidu.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                testHttp();
+            }
+        }).start();
+    }
+
+    private void testHttp() {
+        try {
+            String resp = HttpUtils.commonGet(URL);
+            if (DEBUG) {
+                Log.d(TAG, "commonGet resp : " + resp);
+            }
+        } catch (IOException e) {
+            if (DEBUG) {
+                Loghelper.w(TAG, "http exception", e);
+            }
+        }
     }
 }
