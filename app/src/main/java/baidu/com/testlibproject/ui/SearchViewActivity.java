ca28d9efebbf2397bc8ca7abc8932f2ba610fe57
weimuhua <weimuhua0812@126.com>
2015-11-21 23:31:39 +0800

    change: complete test SearchView.

--- app/src/main/java/baidu/com/testlibproject/ui/SearchViewActivity.java
+++ app/src/main/java/baidu/com/testlibproject/ui/SearchViewActivity.java
@@ -2,14 +2,50 @@ package baidu.com.testlibproject.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import baidu.com.testlibproject.R;
 
-public class SearchViewActivity extends Activity {
+public class SearchViewActivity extends Activity implements SearchView.OnQueryTextListener {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_view_layout);
+        initView();
+    }
+
+    private void initView() {
+        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setQueryHint(getString(R.string.search));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
+
+        mListView = (ListView) findViewById(R.id.search_view_listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.main_activity_item,
+                getResources().getStringArray(R.array.search_view_arr));
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, "your choose is : " + query, Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText);
+        }
+        return true;
     }
 }
