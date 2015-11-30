05d69a1eaaa9a3f6b79e555fa6a1e1c3a45b2279
weimuhua <weimuhua0812@126.com>
2015-11-30 23:17:34 +0800

    "change : finish Activity after sendTextMessage."

--- app/src/main/java/baidu/com/testlibproject/service/SmsMgrActivity.java
+++ app/src/main/java/baidu/com/testlibproject/service/SmsMgrActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 
 import baidu.com.testlibproject.R;
 
-public class SmsMgrActivity extends Activity {
+public class SmsMgrActivity extends Activity implements Handler.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,17 @@ public class SmsMgrActivity extends Activity {
         PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(), 0);
         sm.sendTextMessage("+8618500388463", null, "I love you! from Android code !", pi, null);
         Toast.makeText(this, "send message to +8618500388463!", Toast.LENGTH_SHORT).show();
+        Handler handler = new Handler(this);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 5000);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        return false;
     }
 }
