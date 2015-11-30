910cf3b54339f24b3701bd99eb6b6545a0c05a68
weimuhua <weimuhua0812@126.com>
2015-11-30 23:12:51 +0800

    "change : complete test SmsManager sendTextMessage."

--- app/src/main/java/baidu/com/testlibproject/service/SmsMgrActivity.java
+++ app/src/main/java/baidu/com/testlibproject/service/SmsMgrActivity.java
@@ -1,7 +1,11 @@
 package baidu.com.testlibproject.service;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
 
 import baidu.com.testlibproject.R;
 
@@ -11,5 +15,13 @@ public class SmsMgrActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sms_mgr_layout);
+        sendMessage();
+    }
+
+    private void sendMessage() {
+        SmsManager sm = SmsManager.getDefault();
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(), 0);
+        sm.sendTextMessage("+8618500388463", null, "I love you! from Android code !", pi, null);
+        Toast.makeText(this, "send message to +8618500388463!", Toast.LENGTH_SHORT).show();
     }
 }
