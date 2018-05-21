c5ad0ab5bf9d069fb2c10593ca666e86fdd8b58e
weimuhua <weimuhua0812@126.com>
2018-05-21 21:16:39 +0800

    添加ConstraintLayout测试代码

--- app/src/main/java/baidu/com/testlibproject/ui/ConstrainLayoutActivity.java
+++ app/src/main/java/baidu/com/testlibproject/ui/ConstrainLayoutActivity.java
@@ -1,15 +1,81 @@
 package baidu.com.testlibproject.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import baidu.com.testlibproject.R;
 
 public class ConstrainLayoutActivity extends Activity {
 
+    private static final String[] TEST_PROGRAM_ARRAY = {
+            "相对位置demo",
+            "Margin demo",
+            "bias demo",
+            "ratio demo",
+            "chains demo",
+            "guideline demo"
+    };
+
+    private Spinner mTestSpinner;
+    private TextView mSpinnerTv;
+    private FrameLayout mFrameLayout;
+
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constrain_layout);
+        mContext = this;
+
+        mTestSpinner = findViewById(R.id.spinner);
+        mSpinnerTv = findViewById(R.id.spinner_tv);
+        mFrameLayout = findViewById(R.id.frame_layout);
+
+        mSpinnerTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTestSpinner.performClick();
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, TEST_PROGRAM_ARRAY);
+        mTestSpinner.setAdapter(adapter);
+        mTestSpinner.setSelection(0);
+        mTestSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSpinnerTv.setText(TEST_PROGRAM_ARRAY[position]);
+                mFrameLayout.removeAllViews();
+                if (id == 0) {
+                    View subView = LayoutInflater.from(mContext).inflate(R.layout.layout_constraint_basic, mFrameLayout, false);
+                    mFrameLayout.addView(subView);
+                } else if (id == 1) {
+
+                } else if (id == 2) {
+
+                } else if (id == 3) {
+
+                } else if (id == 4) {
+                    View subView = LayoutInflater.from(mContext).inflate(R.layout.layout_constraint_chains, mFrameLayout, false);
+                    mFrameLayout.addView(subView);
+                } else if (id == 5) {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mSpinnerTv.setText("");
+            }
+        });
     }
 }
