2c533d0ef7fd9fa6abc51934486193c000c34f67
weimuhua <weimuhua0812@126.com>
2019-10-29 00:21:24 +0800

    add coroutinesExample5.

--- app/src/main/java/baidu/com/testlibproject/coroutines/CoroutinesExample1.kt
+++ app/src/main/java/baidu/com/testlibproject/coroutines/CoroutinesExample1.kt
@@ -1,10 +1,7 @@
 package baidu.com.testlibproject.coroutines
 
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 const val TAG = "CoroutinesExample"
 
@@ -27,4 +24,40 @@ fun coroutinesExample2() {
     }
     Log.d(TAG,"3")
     Log.d(TAG,"4")
+}
+
+fun coroutinesExample3() {
+    repeat(100_000) {
+        // 启动大量的协程
+        GlobalScope.launch {
+            delay(200L)
+            Log.d(TAG, "currentThread: ${Thread.currentThread().name}")
+        }
+    }
+}
+
+fun coroutinesExample4() {
+    Log.d(TAG,"1")
+    val job = GlobalScope.launch(context = Dispatchers.Main, start = CoroutineStart.LAZY) {
+        delay(1000L)
+        Log.d(TAG,"2")
+        Log.d(TAG, "currentThread: ${Thread.currentThread().name}")
+    }
+    Log.d(TAG,"3")
+    job.start() // 如果不调用这个方法，那么job的协程体就不会被调度执行
+    Log.d(TAG,"4")
+}
+
+@ExperimentalCoroutinesApi
+fun coroutinesExample5() {
+    Log.d(TAG,"1")
+    GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
+        Log.d(TAG,"2")
+        Log.d(TAG, "currentThread: ${Thread.currentThread().name}")
+        delay(1000L)
+        Log.d(TAG,"3")
+        Log.d(TAG, "currentThread: ${Thread.currentThread().name}")
+    }
+    Log.d(TAG,"4")
+    Log.d(TAG,"5")
 }
