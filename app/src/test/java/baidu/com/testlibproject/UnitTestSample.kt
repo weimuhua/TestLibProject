3031c8d68e294710727196513fe838a164b204ca
weimuhua <weimuhua0812@126.com>
2021-01-04 10:40:57 +0800

    feat: 升级robolectric至4.5-alpha-3以支持compileSdk 30

--- app/src/test/java/baidu/com/testlibproject/UnitTestSample.kt
+++ app/src/test/java/baidu/com/testlibproject/UnitTestSample.kt
@@ -1,18 +1,15 @@
 package baidu.com.testlibproject
 
-import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import baidu.com.testlibproject.test.ClassUnderTest
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 private const val FAKE_STRING = "HELLO_WORLD"
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [Build.VERSION_CODES.P])
 class UnitTestSample {
 
     @Test
