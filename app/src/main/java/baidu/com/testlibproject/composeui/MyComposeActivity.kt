4770f6d6ac29a68278bc1a01017254f2c119c7f9
weimuhua <weimuhua0812@126.com>
2021-10-31 20:56:19 +0800

    feat: add compose LazyColumn demo

--- app/src/main/java/baidu/com/testlibproject/composeui/MyComposeActivity.kt
+++ app/src/main/java/baidu/com/testlibproject/composeui/MyComposeActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,7 +13,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import baidu.com.testlibproject.composeui.ui.theme.TestLibProjectTheme
@@ -28,7 +25,12 @@ class MyComposeActivity : ComponentActivity() {
             TestLibProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MessageCard(Message("wayne", "this is my way"))
+                    val columnSize = 100
+                    val messages = mutableListOf<Message>()
+                    for (i in 0 until columnSize) {
+                        messages.add(i, Message("wayne", "this is my way"))
+                    }
+                    MessageList(messages)
                 }
             }
         }
@@ -37,7 +39,11 @@ class MyComposeActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row() {
+    Row(
+        modifier = Modifier
+            .padding(5.dp)
+            .width(IntrinsicSize.Max)
+    ) {
         Image(
             painter = rememberImagePainter("http://thirdqq.qlogo.cn/qqapp/101490787/2293D757B1A1F326A620663724D8E1BE/640"),
             contentDescription = null,
@@ -48,7 +54,7 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.size(10.dp))
 
-        Column() {
+        Column {
             Text(text = msg.author)
             Spacer(modifier = Modifier.size(5.dp))
             Text(text = msg.body)
@@ -56,6 +62,17 @@ fun MessageCard(msg: Message) {
     }
 }
 
+@Composable
+fun MessageList(messages: List<Message>) {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(messages.size) { index ->
+            MessageCard(msg = messages[index])
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
