e22b70d900a3dd840a37be7c43d92e3895ba413e
weimuhua <weimuhua0812@126.com>
2023-06-27 10:45:37 +0800

    feat: migrate settings.gradle to settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+        maven(url = "./gradle_repo")
+        maven(url = "https://maven.aliyun.com/repository/google")
+        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://maven.aliyun.com/repository/jcenter")
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+        maven(url = "./gradle_repo")
+        maven(url = "https://maven.aliyun.com/repository/google")
+        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://maven.aliyun.com/repository/jcenter")
+    }
+}
+rootProject.name = "TestLibProject"
+include(":app")
+include(":annotation")
+include(":processor")
+include(":CommonTools")
+include(":flutter_transform_plugin")
+
+project(":CommonTools").projectDir = file("submodules/CommonTools")
+project(":processor").projectDir = file("submodules/processor")
+project(":annotation").projectDir = file("submodules/annotation")
+project(":flutter_transform_plugin").projectDir = file("submodules/flutter_transform_plugin")
