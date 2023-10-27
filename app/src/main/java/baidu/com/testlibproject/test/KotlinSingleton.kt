badaf8fb3de0edff765b5d981edb14b074d47013
weimuhua <weimuhua0812@126.com>
2023-10-27 13:35:37 +0800

    feat: 简化单例的写法

--- app/src/main/java/baidu/com/testlibproject/test/KotlinSingleton.kt
+++ app/src/main/java/baidu/com/testlibproject/test/KotlinSingleton.kt
@@ -5,22 +5,13 @@ import android.content.Context
 class KotlinSingleton private constructor(private val context: Context) {
 
     companion object {
+        @Volatile
         private var instance: KotlinSingleton? = null
 
         fun getInstance(context: Context): KotlinSingleton {
-            val i = instance
-            if (i != null) {
-                return i
-            }
-
-            return synchronized(this) {
-                val i2 = instance
-                if (i2 != null) {
-                    i2
-                } else {
-                    val created = KotlinSingleton(context)
-                    instance = created
-                    created
+            return instance ?: synchronized(this) {
+                instance ?: KotlinSingleton(context).also {
+                    instance = it
                 }
             }
         }
