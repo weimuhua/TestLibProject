36691d8f1db5e16211f8a257493068fae7830c9a
weimuhua <weimuhua0812@126.com>
2023-12-03 13:19:18 +0800

    feat: add native function

--- submodules/nativelib/src/main/cpp/nativelib.cpp
+++ submodules/nativelib/src/main/cpp/nativelib.cpp
@@ -3,19 +3,18 @@
 
 using namespace std;
 
-extern "C" JNIEXPORT jstring
-JNICALL
+extern "C" jstring
 Java_com_example_nativelib_NativeLib_stringFromJNI(JNIEnv *env, jobject) {
     string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
 
-extern "C" JNIEXPORT jint JNICALL
+extern "C" JNIEXPORT jint
 Java_com_example_nativelib_NativeLib_addNative(JNIEnv* env, jclass thiz, jint a, jint b) {
     return a + b;
 }
 
-extern "C" JNIEXPORT jint JNICALL
+extern "C" JNIEXPORT jint
 Java_com_example_nativelib_NativeLib_subtractNative(JNIEnv* env, jclass thiz, jint a, jint b) {
     return a - b;
 }
